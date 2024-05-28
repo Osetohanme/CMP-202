e22694b26cc171569e1e34c382a9d89fcef96a7e
Osetohanme <129763480+Osetohanme@users.noreply.github.com>
2024-05-28 15:57:25 +0100

    Add files via upload

---
+++ Data Structures/Processor.java
@@ -0,0 +1,14 @@
+public class Processor {
+
+    @Override
+    public String toString() {
+        return "Processor{" +
+                "type='" + type + '\'' +
+                ", noOfcores=" + noOfcores +
+                '}';
+    }
+
+    String type = "Core I9";
+
+    int noOfcores = 8;
+}
